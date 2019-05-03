5fcb5816a10302da1513b40520a50da179213d1f
plinio <plinio.grijalba@grupodigital.co>
2019-05-03 11:54:54 -0500

    resolve bug token

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -3,12 +3,14 @@ const config = require('./../config/config.js');
 
 let checkToken = (req, res, next) => {
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-        
+
     if (token) {
         if (token.startsWith('Bearer ')) {
             // Remove Bearer from string
             token = token.slice(7, token.length);
         }
+        console.log(token);
+        
         jwt.verify(token, config.secret, (err, decoded) => {
             if (err) {
                 return res.json({
